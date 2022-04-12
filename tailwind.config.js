e9c4ee15b42af1bb0065c502f24dfec2685f8b5d
Daniel Pauly Retraubun <nyderetna@gmail.comgit config --global user.email nyderetna@gmail.comgit config --global user.email nyderetna@gmail.com>
2022-04-12 22:51:13 +0700

    portfolio jip

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container : {
+      center : true,
+      padding : '16px',
+    },
+    extend: {
+      colors : {
+        primary : '#d97706',
+        secondary : '#d97706',
+        dark : '#1c1917',
+      },
+      screens : {
+        '2xl' : '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
